e8031fac07997144ac55a1bfd061a1b99a4710e2
PatrycjaWiza <patrycja.wiza@gmail.com>
2022-05-16 19:43:00 +0200

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,2 +1,18 @@
-const counterValue = document.querySelector("#value").textContent;
-console.log(counterValue);
+const counterValue = document.querySelector("#value");
+const addValue = document.querySelector('[data-action="increment"]');
+const minusValue = document.querySelector('[data-action="decrement"]');
+
+let a = 1;
+
+const addLog = () =>{
+    a++;
+    counterValue.innerHTML = a;
+};
+
+const minusLog = () =>{
+    a--;
+    counterValue.innerHTML = a;
+};
+
+addValue.addEventListener("click", addLog);
+minusValue.addEventListener("click", minusLog);
