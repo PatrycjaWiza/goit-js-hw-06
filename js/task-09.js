a3b38a8a9e65907782b59bda2fa6f369e9a2bdba
PatrycjaWiza <patrycja.wiza@gmail.com>
2022-05-16 23:50:30 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+console.log(getRandomHexColor());
+const body = document.querySelector("body");
+const button = document.querySelector("button");
+const color = document.querySelector(".color");
+
+const handleEvent = () => {
+  body.style.backgroundColor = getRandomHexColor();
+  color.innerHTML = getRandomHexColor();
+};
+button.addEventListener("click", handleEvent);
