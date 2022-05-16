a7487b8b3febc80e919726f2c39d85c8420a2bd8
PatrycjaWiza <patrycja.wiza@gmail.com>
2022-05-16 23:16:55 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,14 @@
+const frm = document.querySelector(".login-form");
+frm.addEventListener("submit", handleEvent);
 
+function handleEvent(e) {
+  e.preventDefault();
+  const {
+    elements: { email, password },
+  } = e.currentTarget;
+  if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fileds!");
+  }
+  console.log(`Login:${email.value}, Password:${password.value}`);
+  e.currentTarget.reset();
+}
