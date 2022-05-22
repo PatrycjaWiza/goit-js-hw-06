4c2204f703f9814ea81f007aa999d83674ead671
PatrycjaWiza <patrycja.wiza@gmail.com>
2022-05-22 16:04:44 +0200

    INITIAL
    basic constant,
    remove function,
    create function
    2 do:
    adds 10px with each counter &
    div counter = amount

--- js/task-01.js
+++ js/task-01.js
@@ -5,5 +5,3 @@ const elementName = document.querySelectorAll("li.item").forEach((e) => {
   console.log(`Category: ${e.querySelector("h2").textContent}`);
   console.log(`Elements: ${e.querySelectorAll("li").length}`);
 });
-
-// brakuje elements count!!!
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,27 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const divBoxes = document.querySelector("#boxes");
+const create = document.querySelector("button[data-create]");
+const destroy = document.querySelector("button[data-destroy]");
+const value = document.querySelector("#controls input");
+
+create.addEventListener("click", createBoxes);
+
+function createBoxes(amount) {
+  //create as much divs as amount number TO DO
+  amount = value.value;
+  const boxArr = [];
+  const box = document.createElement("div");
+  //each next elements +"10px" TO DO
+  box.style.width = "30px";
+  box.style.height = "30px";
+  box.style.backgroundColor = getRandomHexColor();
+  boxArr.push(box);
+
+  divBoxes.append(box);
+}
+function destroyBoxes() {
+  divBoxes.innerHTML = "";
+}
+destroy.addEventListener("click", destroyBoxes);
